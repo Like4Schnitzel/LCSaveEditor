0083e1886b1a43758f2eb2ac98a8c90f0857280d
Schnitzel <rw21062006@gmail.com>
2024-04-14 06:30:39 +0200

    initial commit

---
+++ LCSaveEditor.cs
@@ -0,0 +1,47 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LobbyCompatibility.Attributes;
+using LobbyCompatibility.Enums;
+
+namespace LCSaveEditor;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("BMX.LobbyCompatibility", BepInDependency.DependencyFlags.HardDependency)]
+[LobbyCompatibility(CompatibilityLevel.ClientOnly, VersionStrictness.None)]
+public class LCSaveEditor : BaseUnityPlugin
+{
+    public static LCSaveEditor Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger { get; private set; } = null!;
+    internal static Harmony? Harmony { get; set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Instance = this;
+
+        Patch();
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+    }
+
+    internal static void Patch()
+    {
+        Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        Logger.LogDebug("Patching...");
+
+        Harmony.PatchAll();
+
+        Logger.LogDebug("Finished patching!");
+    }
+
+    internal static void Unpatch()
+    {
+        Logger.LogDebug("Unpatching...");
+
+        Harmony?.UnpatchSelf();
+
+        Logger.LogDebug("Finished unpatching!");
+    }
+}
---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LCSaveEditor
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Like4Schnitzel.LCSaveEditor";
+        public const string PLUGIN_NAME = "LCSaveEditor";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
